2c087645f7428fa3f647597a2f5289bdb7e8e6f6
John Aaron Alcoseba <jqalcoseba@up.edu.ph>
2019-05-19 03:24:44 +0800

    fixed function overloading

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ import os
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
-def recentPayday(date, payday):
+def recentPayday(date=datetime.now(), payday=24):
     thisPayday = date - relativedelta(months=0, day=payday)
     lastPayday = date - relativedelta(months=1, day=payday)
     if (date - thisPayday).days >= 0:
         return thisPayday
     return lastPayday
-
-def recentPayday(payday):
-    return recentPayday(datetime.now(), payday)
 
 def strToDate(str):
     return datetime.strptime(str, '%Y/%m/%d')
