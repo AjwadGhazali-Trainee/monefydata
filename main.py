be6ca7a7f248913281ad01ccb2d335c075177043
John Aaron Alcoseba <jqalcoseba@up.edu.ph>
2019-05-19 11:10:20 +0800

    added budget structure and basic selection.

--- main.py
+++ main.py
@@ -9,13 +9,28 @@ def recentPayday(date=datetime.now(), payday=24):
     thisPayday = date - relativedelta(months=0, day=payday)
     lastPayday = date - relativedelta(months=1, day=payday)
     if (date - thisPayday).days >= 0:
-        return thisPayday
-    return lastPayday
-
-def strToDate(str):
-    return datetime.strptime(str, "%Y/%m/%d")
+        return thisPayday.replace(hour=0,minute=0,second=0,microsecond=0)
+    return lastPayday.replace(hour=0,minute=0,second=0,microsecond=0)
 
 def getMostRecentCsv():
     return max(glob.iglob("*.csv"), key=os.path.getmtime)
 
-print(getMostRecentCsv())
+budgetStructure = {
+    'daily' : 0.5,
+    'wants' : 0.23,
+    'emergency' : 0.15
+}
+
+monthly = ['House', 'Laundry', 'Phone', 'Netflix', 'Spotify']
+daily = ['Food', 'Transport', 'Eating out', 'Communications', 'Health']
+wants = ['Gift', 'Gifts', 'Entertainment', 'Clothes', 'Sports', 'Haircut', 'Book', 'Pets']
+
+df = pd.read_csv(getMostRecentCsv())
+
+recentPayday = recentPayday()
+
+df['date'] = pd.to_datetime(format="%d/%m/%Y",arg=df['date'])
+
+df = df[df['date'] >= recentPayday]
+
+print(df[df['category'].isin(daily)])
