68c8c5c326aa4fdd8164ef129a001f346e495adc
John Aaron <johnaaron658@gmail.com>
2019-05-19 17:55:35 +0800

    Broken down to modules

---
+++ budget.py
@@ -0,0 +1,5 @@
+class Budget:
+	def __init__(self, name='new budget',  alloc=0, categoryList=[]):
+		self.name = name
+		self.alloc = alloc
+		self.list = categoryList
--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-import pandas as pd
-import numpy as np
-import glob
-import os
-from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
-
-def recentPayday(date=datetime.now(), payday=24):
-    thisPayday = date - relativedelta(months=0, day=payday)
-    lastPayday = date - relativedelta(months=1, day=payday)
-    if (date - thisPayday).days >= 0:
-        return thisPayday.replace(hour=0,minute=0,second=0,microsecond=0)
-    return lastPayday.replace(hour=0,minute=0,second=0,microsecond=0)
-
-def getMostRecentCsv():
-    return max(glob.iglob("*.csv"), key=os.path.getmtime)
+import budget as bd
+import processing as ps
 
 budgetStructure = {
     'daily' : 0.5,
@@ -24,13 +10,3 @@ budgetStructure = {
 monthly = ['House', 'Laundry', 'Phone', 'Netflix', 'Spotify']
 daily = ['Food', 'Transport', 'Eating out', 'Communications', 'Health']
 wants = ['Gift', 'Gifts', 'Entertainment', 'Clothes', 'Sports', 'Haircut', 'Book', 'Pets']
-
-df = pd.read_csv(getMostRecentCsv())
-
-recentPayday = recentPayday()
-
-df['date'] = pd.to_datetime(format="%d/%m/%Y",arg=df['date'])
-
-df = df[df['date'] >= recentPayday]
-
-print(df[df['category'].isin(daily)])
