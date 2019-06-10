99f94ec7f98397c0336e2e73a9549b693fc55acd
John Aaron <johnaaron658@gmail.com>
2019-06-10 16:08:33 +0800

    Updated budget structure map.

--- budget.py
+++ budget.py
@@ -1,7 +1,42 @@
 from config import *
 
-class Budget:
-	def __init__(self, name=COMMON_DEFAULT_BUDGET_NAME,  alloc=0, categoryList=[]):
-		self.name = name
-		self.alloc = alloc
-		self.categoryList = categoryList
+payday = 24
+
+budgetList =  [
+			{
+				'name': 'Savings',
+				'percentage': 0.12,
+				'frequency': MONTHLY,
+				'prededuct': 0,
+				'categoryList': [
+				
+				]
+			},
+			{
+				'name': 'Emergency',
+				'percentage': 0.15,
+				'frequency': MONTHLY,
+				'prededuct': 0,				
+				'categoryList': [
+				
+				]
+			},
+			{
+				'name': 'Wants',
+				'percentage': 0.23,
+				'frequency': MONTHLY,
+				'prededuct': 0,
+				'categoryList': [
+				
+				]
+			},
+			{
+				'name': 'Daily',
+				'percentage': 0.5,
+				'frequency': DAILY,
+				'prededuct': 3500+269,
+				'categoryList': [
+				
+				]
+			}
+		]
